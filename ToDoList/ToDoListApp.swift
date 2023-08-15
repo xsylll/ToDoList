1405dd728bdcdf0e6906f14c3f245e4061e9067a
kayla saniei <sankayla13@gmail.com>
2023-08-15 10:22:15 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by kayla saniei on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
